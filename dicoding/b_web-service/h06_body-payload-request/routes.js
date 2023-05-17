5028652b71a5b49cfdd5179e6cacd69b7fcd2522
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2023-05-17 10:38:36 +0700

    payload request

---
+++ dicoding/b_web-service/h06_body-payload-request/routes.js
@@ -0,0 +1,62 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        }
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        }
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        }
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        }
+    },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const {name = 'stranger'} = request.params;
+            const {lang} = request.query;
+
+            if(lang === 'id'){
+                return `Hello, ${name}!`;
+            }
+            
+            return `Hello, ${name}!`;
+        }
+    },
+    // ex payload request login
+    {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const {username, password} = request.payload;
+            return `Welcome ${username}`;
+        }
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        }
+    },
+]
+
+module.exports = routes;
