510e6cafcab1b5861c34809606466cbba284a4df
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2023-05-17 13:23:04 +0700

    http server note restful api

--- dicoding/b_web-service/notes-app-backend/src/server.js
+++ dicoding/b_web-service/notes-app-backend/src/server.js
@@ -1,1 +1,13 @@
-console.log('RESTful API gokil bersama');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
